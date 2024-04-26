d9454bbf9f86afc8a6c87bf3bb6e0d3303fc3f7a
sheikhsulaimansony <sheikhsulaimansony@outlook.com>
2024-04-26 17:50:21 +0600

    system theme

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Weather App',
-      theme: darkMode,
+      theme: lightMode,
+      darkTheme: darkMode,
       home: const WeatherPage(),
     );
   }
